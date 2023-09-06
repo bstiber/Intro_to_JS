014ce31d34cc37ac8a34126b5a157220ab781a57
Bill Stiber <billstiber@gmail.com>
2023-09-06 14:12:37 -0400

    rename

--- 02_functions/stringCodeChallenge.js
+++ 02_functions/stringCodeChallenge.js
@@ -1,5 +1,4 @@
 // String Code Challenge:
-// Declare a new string variable with name coder and set value to 'I am a good programmer'
 // Find out the type of the variable coder
 // Find out the length of the variable coder
 // Convert to all uppercase
@@ -10,3 +9,9 @@
 // Split the variable into two variable at `good` word 
 // Review all possible action we can do on a string from this doc
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+
+// first attempt
+
+// Declare a new string variable with name coder and set value to 'I am a good programmer'
+let coder = 'I am a good programmer';
+console.log(coder);
